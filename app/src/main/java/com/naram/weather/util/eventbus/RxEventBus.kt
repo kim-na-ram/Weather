caf2d71179f2836178c72c737be2b4016375f3cc
NARAM <32188154+kim-na-ram@users.noreply.github.com>
2022-06-24 11:21:07 +0900

    RxEventBus 수정

--- app/src/main/java/com/naram/weather/util/eventbus/RxEventBus.kt
+++ app/src/main/java/com/naram/weather/util/eventbus/RxEventBus.kt
@@ -1,19 +1,21 @@
 package com.naram.weather.util.eventbus
 
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 object RxEventBus {
-    private val events = HashMap<Event, PublishSubject<Any>>()
+    private val events = HashMap<Event, BehaviorSubject<Any>>()
     val mutableEvents get() = events
 
     fun post(eventName: Event, event: Any) {
+        if(events[eventName] == null)
+            events[eventName] = BehaviorSubject.create()
         events[eventName]?.onNext(event)
     }
 
     inline fun <reified T> listen(eventName: Event): Observable<T> {
         if (mutableEvents.containsKey(eventName).not()) {
-            mutableEvents[eventName] = PublishSubject.create()
+            mutableEvents[eventName] = BehaviorSubject.create()
         }
         return mutableEvents[eventName]!!.ofType(T::class.java)
     }
